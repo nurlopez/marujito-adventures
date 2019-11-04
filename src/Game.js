45c16d4c520685e236829a32bce190e341211119
nurlopez <nlopezcolom@gmail.com>
2019-11-04 17:58:18 +0100

    Granny Quotes work. next step: stylings

--- src/Game.js
+++ src/Game.js
@@ -138,7 +138,23 @@ Game.prototype.checkCollisions = function () {
     this.foodSeeds.forEach(function (foodSeed) {
         if (this.player.didCollide(foodSeed)) {
             this.player.addScore();
-
+            if (this.player.score %500 === 0) {
+                var grannyTips = [
+                  'Potato slices on your face, cures headaches',
+                  'Feeling itchy? Olive oil for Eczema',
+                  'Have a little yogurt for that bad breath',
+                  'Ease your hangover with a teaspoon of apple cider vinegar',
+                  'Vodka for stinky feet',
+                  'Stop snoring with a glass of warm milk and tumeric powder',
+                  'Have you had enough to eat? Are you sure? Do you want a sandwich?',
+                  'When I was your age I had to walk to school uphill, both ways!',
+                  'If you can\'t say something nice, don\'t say anything at',
+                  'You catch more flies with honey than with vinegar',
+                  'Don\'t leave until tomorrow what you can do today'
+                  ]
+               this.quote = grannyTips[Math.floor(Math.random() * grannyTips.length)];
+               return this.quote;
+              } 
             foodSeed.y = 0 - foodSeed.size;
         }
     }, this);
